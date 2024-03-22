180632c6f5543ea7d55875d6e45317a972205412
SantiagoBulla <santiagobulla1216@gmail.com>
2024-03-22 15:34:52 -0500

    making the edusity web page resposive to smaller devices

--- edusity/src/components/Navbar/Navbar.jsx
+++ edusity/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from "react-scroll";
 
 const Navbar = () => {
 
     const [sticky, setSticky] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
 
     //if the scroll exceeds 150px then the control variable is changed to true and if conditional rendering applies 
     useEffect(() => {
@@ -14,10 +16,14 @@ const Navbar = () => {
         })
     }, [])
 
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" srcset="" className='logo' />
-            <ul>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
@@ -25,6 +31,7 @@ const Navbar = () => {
                 <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
                 <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
+            <img src={menu_icon} alt="menu_icon" className='menu-icon' onClick={toggleMenu} />
         </nav>
     )
 }
